92386a117e71c25adb6f70aac3da590d5d37332c
shakeel159 <77754753+shakeel159@users.noreply.github.com>
2021-12-15 11:55:39 -0600

    Update and rename main to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
-#include <iostream>
+
+include <iostream>
 //#include "logic.cpp"
 #include "logic.h"
 #include <ctime>
 
-using namespace std;
+//using namespace std;
 
 int main()
 {
@@ -11,31 +12,40 @@ int main()
     int width=5;
     int height=5;
     int shipCount = 4;
+
+    // Coordinates{
+    //     int x,
+    //     int y;
+    // };
     
     bool **board = create_board(height, width);
     
     for (int i = 0; i < width; i++) {
         bool *row = board[i];
         for(int j=0;j<width;j++) {
-            cout << row[j] << endl;
+            std::cout << row[j] << std::endl;
             
         }
-        cout << "-----------" << endl;
+        std::cout << "-----------" << std::endl;
     }
     
 
     place_ships(board, shipCount, width, height);
 
-    cout << "Genarating new board" << endl;
+    std::cout << "Genarating new board" << std::endl;
 
     //find_ships(board, width, height)
+    //coordinates *find_ships(board,  );
     
     for (int i = 0; i < width; i++) {
         bool *row = board[i];
         for(int j=0;j<width;j++) {
-            cout << row[j] << endl;
+            std::cout << row[j] << std::endl;
             
         }
-        cout << "-----------" << endl;
+        std::cout << "-----------" << std::endl;
     }
+
+    find_ships(board, width, height, shipCount);
+    
 };
